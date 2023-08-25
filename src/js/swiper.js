ebd3e77e69927c93e1eaeb8b4c5d1f30d6a670bc
Hossein Pirasbaghi <hosseinpirasbaghi@gmail.com>
2023-08-25 22:19:43 +0330

    swiper js section updated

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,23 +2,20 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
   speed: 400,
-  spaceBetween: 100,
+  spaceBetween: 30,
+  centeredSlides:true,
   autoplay: {
-    delay: 5000,
+    delay: 2000,
+    disableOnInteraction: false,
   },
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable:true
   },
-
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
 });
