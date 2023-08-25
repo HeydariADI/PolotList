b8960c2c9cacbbc6e0cc31da87e2ead964ae7067
Hossein Pirasbaghi <hosseinpirasbaghi@gmail.com>
2023-08-25 22:01:56 +0330

    swiper js 02

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,20 +1,24 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
+  // Optional parameters
+  loop: true,
+  speed: 400,
+  spaceBetween: 100,
+  autoplay: {
+    delay: 5000,
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
